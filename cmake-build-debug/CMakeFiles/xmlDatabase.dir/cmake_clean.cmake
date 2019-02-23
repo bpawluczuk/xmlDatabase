9729a56f63ae94a8d3d273302993bc770f5ae72c
bpawluczuk <b.pawluczuk@companion.pl>
2019-02-23 22:11:34 +0100

    init

---
+++ cmake-build-debug/CMakeFiles/xmlDatabase.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/xmlDatabase.dir/main.cpp.o"
+  "xmlDatabase.pdb"
+  "xmlDatabase"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/xmlDatabase.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/xmlDatabase.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for xmlDatabase.
+# This may be replaced when dependencies are built.
